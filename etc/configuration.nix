8d12d1ddeed74530eebc0aafee42de530c5118a9
Emil Wojda <ewojda@posteo.net>
2023-01-06 18:02:58 +0100

    Replaced deprecated nix flakes package

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -351,7 +351,7 @@ rec {
 
   # Nix flakes
    nix = {
-     package = pkgs.nixFlakes;
+     package = pkgs.nixVersions.stable;
      extraOptions = ''
        experimental-features = nix-command flakes
      '';
