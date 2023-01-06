b62fd5c6d9f27e01036342b951485676cdc8f88a
Emil Wojda <ewojda@posteo.net>
2023-01-06 18:17:40 +0100

    -firefox nightly

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -369,17 +369,6 @@ rec {
 
   nixpkgs.config.allowUnfree = true;
 
-  # Firefox nightly
-  nixpkgs.overlays =
-    let
-      # Change this to a rev sha to pin
-      moz-rev = "master";
-      moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
-      nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
-    in [
-      nightlyOverlay
-    ];
-
   # Nix flakes
    nix = {
      package = pkgs.nixVersions.stable;
